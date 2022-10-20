1761651a62154b75c26d751b4bff3ed566aa81d3
Ant <huertinez.family@outlook.com>
2022-10-19 19:56:25 -0700

    get the task to display, made sure to get the name and type to a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,28 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
   event.preventDefault();
 
-  console.log(event)
   var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  console.log(taskTypeInput);
 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  // taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // listItemEl.textContent = taskNameInput;
+  
   tasksToDoEl.appendChild(listItemEl);
 
+  console.dir(listItemEl);
   console.dir(taskNameInput);
 };
 
